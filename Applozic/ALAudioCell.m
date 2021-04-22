d706c97c22bfbe64193d483c6180c1d3cbf46da3
AlexProsperity <avelikotckiy.prosperity@gmail.com>
2021-04-22 13:53:50 +0300

    fix color button

--- Applozic/ALAudioCell.m
+++ Applozic/ALAudioCell.m
@@ -249,6 +249,7 @@ static CGFloat const USER_PROFILE_HEIGHT = 36;
         {
             self.progresLabel.alpha = 0;
         }
+        [self.mDowloadRetryButton setBackgroundColor: [UIColor colorWithRed:33.0/255 green:120.0/255 blue:103.0/255 alpha:1]];
     } else
     {
 
@@ -336,7 +337,7 @@ static CGFloat const USER_PROFILE_HEIGHT = 36;
             self.mDowloadRetryButton.alpha = 1;
             [self.mDowloadRetryButton setImage:[ALUIUtilityClass getImageFromFramworkBundle:@"UploadiOS2.png"] forState:UIControlStateNormal];
         }
-        
+        [self.mDowloadRetryButton setBackgroundColor: [UIColor colorWithRed:33.0/255 green:120.0/255 blue:103.0/255 alpha:1]];
     }
 
     self.frontView.frame = self.mBubleImageView.frame;
